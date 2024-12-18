960851534806b5a5b267be44b87fa0bbe81367b1
raxmonovrx <rafarruxbek@gmail.com>
2024-12-18 20:44:04 +0500

    TailwindCSS Nike Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<div className='bg-blue-500 text-white p-4 text-center rounded-lg'>
+			<h1 className='text-2xl font-bold'>Hello, Tailwind CSS!</h1>
+		</div>
+	)
+}
