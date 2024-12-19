96404b3ac1d5ea3325e4573dc447779a4abf1d70
raxmonovrx <rafarruxbek@gmail.com>
2024-12-19 21:39:46 +0500

    Add ReviewCard component and implement CustomerReviews section; create robots.txt for SEO and Done All sections

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -5,23 +5,23 @@ import copyrightSign from './copyright-sign.svg'
 import facebook from './facebook.svg'
 import hamburger from './hamburger.svg'
 import instagram from './instagram.svg'
-import support from './support.svg'
 import shieldTick from './shield-tick.svg'
 import star from './star.svg'
+import support from './support.svg'
 import truckFast from './truck-fast.svg'
 import twitter from './twitter.svg'
 
 export {
-    arrowRight,
-    cheveronRight,
-    chevronLeft,
-    copyrightSign,
-    facebook,
-    hamburger,
-    instagram,
-    support,
-    shieldTick,
-    star,
-    truckFast,
-    twitter
+	arrowRight,
+	cheveronRight,
+	chevronLeft,
+	copyrightSign,
+	facebook,
+	hamburger,
+	instagram,
+	shieldTick,
+	star,
+	support,
+	truckFast,
+	twitter,
 }
