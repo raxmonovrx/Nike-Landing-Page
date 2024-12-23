b1d2b3fdb9efd1b75e574baa5fc29d74200db134
FarruXbek Raxmonov <83349809+raxmonovrx@users.noreply.github.com>
2024-12-23 15:55:13 +0500

    github

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [react()],
-	base: '/nike-shoes/', // GitHub Pages repozitoriyasi nomiga mos keladi
+  plugins: [react()],
+  base: '/nike-shoes/', // GitHub Pages repozitoriyasi nomiga mos keladi
 })
